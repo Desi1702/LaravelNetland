77edf16cb6c32a0619e1f1232ceecd824ef7510d
Desi1702 <2093155@talnet.nl>
2025-01-16 15:31:11 +0100

    .

--- netland/app/Http/Controllers/MediaController.php
+++ netland/app/Http/Controllers/MediaController.php
@@ -16,9 +16,7 @@ class MediaController extends Controller
 
         return view('search', ['results' => $results, 'query' => $query]);
     }
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function show(Media $media)
     {
         return view('details', ['item' => $media, 'type' => 'media']);
@@ -28,45 +26,5 @@ class MediaController extends Controller
     {
         $media = Media::all();
         return view('index', ['items' => $media, 'type' => 'media']);
-    }
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
